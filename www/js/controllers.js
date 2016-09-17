e1413e4608a3cb3d22117b2f6a185b37a32e3bd9
Michał Wlaź <michal.wlaz@orange.com>
2016-09-17 11:17:00 +0200

    nowe funkcje

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,16 +3,17 @@
 angular.module('app')
 
   .controller('HomeController', function ($scope,partyData, $ionicModal, $state) {
-    $scope.partyData = partyData;
+    $scope.partyData = partyData.get();
 
     $ionicModal.fromTemplateUrl('templates/modal.html', {
       scope: $scope,
-      animation: 'slide-in-up'
+      animation: 'slide-in-left'
     }).then(function(modal) {
       $scope.modal = modal;
     });
     $scope.seriouslyStartTheParty = function() {
       $scope.modal.hide();
+      partyData.set($scope.partyData);
       $state.go('party');
     };
     // Cleanup the modal when we're done with it!
@@ -21,8 +22,13 @@ angular.module('app')
     });
 
     $scope.letsGetThisPartyStarted = function openPartyModal() {
-      partyData.start = new Date();
+      $scope.partyData.start = new Date().toLocaleString();
       $scope.modal.show();
+    };
+
+    $scope.clearAndLeave = function(){
+      $scope.partyData = {name: '', start: '', end: '', limit: {type: '', quantity: '', unit: ''},drinks:[]};
+      $scope.modal.hide();
     };
   })
 
@@ -33,14 +39,59 @@ angular.module('app')
     $scope.historyData = historyData;
   })
 
-  .controller('SettingsController', function ($scope, settings, $rootScope) {
-    $scope.settings = settings;
+  .controller('SettingsController', function ($scope, settings, $rootScope, $state) {
+    $scope.settings = settings.get();
     $scope.keyboardOpen = $rootScope.keyboardOpen;
+    $scope.submit = function(){
+      settings.set($scope.settings);
+      $state.go('home');
+    }
   })
 
   .controller('AboutController', function ($scope) {
   })
 
-  .controller('PartyController', function ($scope,partyData) {
-    $scope.partyData = partyData;
+  .controller('PartyController', function ($log,$scope,partyData,lastDrink, $ionicModal, definedDrinks) {
+    $scope.partyData = partyData.get();
+    $scope.lastDrink = lastDrink.get();
+    $log.debug(lastDrink.get());
+    $scope.limit = null;
+    $scope.selectedCategory = 'All';
+    $scope.selectedDrink = null;
+    $scope.newDrink = {name:'',category:'',volume:0,alcohol:0};
+    $scope.mockupDrink = {name:'Regular beer',category: 'beer',volume: '500',alcohol: '0.04'};
+
+    $scope.duplicateDrink = function (){
+      partyData.addDrink($scope.lastDrink);
+    };
+
+    $ionicModal.fromTemplateUrl('templates/addDrinkModal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+
+    $scope.addDrinkModal = function(){
+      $scope.modal.show();
+    };
+
+    $scope.$on('$destroy', function() {
+      $scope.modal.remove();
+    });
+
+    $scope.addSavedDrink = function(drink){
+      partyData.addDrink(drink);
+      lastDrink.set(drink);
+      $scope.lastDrink = drink;
+      $scope.modal.hide();
+    };
+
+    $scope.addNewDrink = function(drink){
+      definedDrinks.addDrink(drink);
+      partyData.addDrink(drink);
+      lastDrink.set(drink);
+      $scope.lastDrink = drink;
+      $scope.modal.hide();
+    };
   });
--- www/js/services.js
+++ www/js/services.js
@@ -2,7 +2,101 @@
 
 angular.module('app')
 
-  .value('settings', {sex: 'none', height: 0, weight: 0, address: ''})
-  .value('partyData', {name: '', start: '', end: '', limit: {type: '', quantity: '', unit: ''},drinks:[]})
-  .value('definedDrinks',[{name:'Beer', volume: '500', alcohol: '0.04'},{name:'Wine', volume: '150', alcohol: '0.12'},{name:'Vodka', volume: '50', alcohol: '0.4'}])
-  .value('historyData', [{name: 'My Birthday', start: '09.07.2016, 18:00', end: '09.07.2016, 24:00', drinks: [{name: 'Beer', alcohol: '0.04', volume: '500'},{name: 'Beer', alcohol: '0.04', volume: '500'}]}, {name: 'Graduation Party', start: '25.06.2016, 17:00', end: '25.05.2016, 22:30', drinks: [{name: 'Wine', alcohol: '0.12', volume: '150'}]}]);
+  .factory('settings', function () {
+    return {
+      get: function () {
+        var tmp = window.localStorage.getItem('settings');
+        return tmp ? angular.fromJson(tmp) : {sex: 'none', height: 0, weight: 0, address: ''};
+      },
+      set: function (settingsObject) {
+        window.localStorage.setItem('settings', angular.toJson(settingsObject));
+      }
+    }
+  })
+  .factory('partyData', function () {
+    return {
+      get: function () {
+        var tmp = window.localStorage.getItem('partyData');
+        return tmp ? angular.fromJson(tmp) : {
+          name: '',
+          start: '',
+          end: '',
+          limit: {type: '', quantity: '', unit: ''},
+          drinks: []
+        };
+      },
+      set: function (partyObject) {
+        window.localStorage.setItem('partyData', angular.toJson(partyObject));
+      },
+      addDrink: function (drinkObject) {
+        var tmp = angular.fromJson(window.localStorage.getItem('partyData'));
+        tmp.drinks.push(drinkObject);
+        window.localStorage.setItem('partyData', angular.toJson(tmp));
+      }
+    }
+  })
+  .factory('definedDrinks', function () {
+    return {
+      get: function () {
+        var tmp = window.localStorage.getItem('definedDrinks');
+        return tmp ? angular.fromJson(tmp) : [{name: 'Beer', category:'Beer', volume: '500', alcohol: '0.04'}, {
+          name: 'Wine',
+          category: 'Wine',
+          volume: '150',
+          alcohol: '0.12'
+        }, {name: 'Vodka', category: 'String alcohols', volume: '50', alcohol: '0.4'}];
+      },
+      set: function (drinksObject) {
+        window.localStorage.setItem('definedDrinks', angular.toJson(drinksObject));
+      },
+      addDrink: function (drinkObject) {
+        var tmp = angular.fromJson(window.localStorage.getItem('definedDrinks'));
+        tmp.drinks.push(drinkObject);
+        window.localStorage.setItem('definedDrinks', angular.toJson(tmp));
+      }
+    }
+  })
+  .factory('historyData', function ($log) {
+    return {
+      get: function () {
+        var tmp = window.localStorage.getItem('historyData');
+        return tmp ? angular.fromJson(tmp) : [{
+          name: 'My Birthday',
+          start: '09.07.2016, 18:00',
+          end: '09.07.2016, 24:00',
+          drinks: [{name: 'Beer', alcohol: '0.04', volume: '500'}, {name: 'Beer', alcohol: '0.04', volume: '500'}]
+        }, {
+          name: 'Graduation Party',
+          start: '25.06.2016, 17:00',
+          end: '25.05.2016, 22:30',
+          drinks: [{name: 'Wine', alcohol: '0.12', volume: '150'}]
+        }]
+      },
+      set: function (drinkObject) {
+        window.localStorage.setItem('historyData', angular.toJson(drinkObject));
+      },
+      addParty: function (partyObject) {
+        var tmp = angular.fromJson(window.localStorage.getItem('historyData'));
+        tmp.drinks.push(partyObject);
+        window.localStorage.setItem('historyData', angular.toJson(tmp));
+      },
+      removeParty: function (partyObject){
+        $log.err('Method not implemented!!');
+        throw new DOMException();
+      }
+    }
+  })
+  .factory('lastDrink', function ($log) {
+    return {
+      get: function () {
+        var tmp = window.localStorage.getItem('lastDrink');
+        $log.debug(tmp);
+        var fromjson = angular.fromJson(tmp);
+        $log.debug(fromjson);
+        return tmp ? {name:fromjson.name,category:fromjson.category,volume:fromjson.volume,alcohol:fromjson.alcohol} : {name:'',alcohol:'',volume:''};
+      },
+      set: function (drinkObject) {
+        window.localStorage.setItem('lastDrink', angular.toJson(drinkObject));
+      }
+    }
+  });
